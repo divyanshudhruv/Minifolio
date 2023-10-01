0ff4a7200048806a547c7ea77ac72e8531a90db2
Mathanraj <madhanmani0601@gmail.com>
2023-10-01 16:36:35 +0530

    Added typingAnimation JS file

---
+++ js/typingAnimation.js
@@ -0,0 +1,7 @@
+// Custom the typed strings array based on your roles
+var typed = new Typed("#role", {
+  strings: ["the Unknown Developer", "a UI/UX Designer", "a Youtuber"],
+  typeSpeed: 210,
+  backSpeed: 40,
+  loop: true,
+});

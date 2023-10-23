a5700341110b2deaa223eabb4a8f3b0a5700aad6
Lolo Maku <markpajao@outlook.com>
2023-10-23 15:43:40 +0800

    added personal touch to the css and js for hacktober2023

--- js/typingAnimation.js
+++ js/typingAnimation.js
@@ -1,6 +1,6 @@
 // Custom the typed strings array based on your roles
 var typed = new Typed("#role", {
-  strings: ["the Unknown Developer", "a UI/UX Designer", "a Youtuber"],
+  strings: ["an Artist", "a Grandfather", "a Front-End Developer"],
   typeSpeed: 210,
   backSpeed: 40,
   loop: true,

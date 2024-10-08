c7e353c522229ce3f1d49cd95030f62ac3310105
Thanmayi Kondreddy <146209969+thanmayi01@users.noreply.github.com>
2024-10-08 11:42:17 +0530

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,11 @@
+// script.js
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  

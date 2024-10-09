11e7f133dfeac7c523e71a4e35edce342af4734f
CodeGallantX <johnayobami77@proton.me>
2024-10-09 17:28:11 +0100

    Created dark mode

---
+++ js/darkMode.js
@@ -0,0 +1,10 @@
+function toggleDarkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+
+    // if (element.classList.contains("dark-mode")) {
+    //     localStorage.setItem("darkMode", "true");
+    // } else {
+    //     localStorage.setItem("darkMode", "false");
+    // }
+}

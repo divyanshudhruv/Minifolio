189eac3e9c3703ca67026bf9252fce970de5c463
Divyanshu Dhruv <divyanshudhruv8@gmail.com>
2024-10-11 16:09:17 +0530

    Fix styling and alignment issues for improved layout

--- js/cursor.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("mousemove", function (e) {
-  const cursor = document.querySelector(".custom-cursor");
-  cursor.style.left = e.clientX + "px";
-  cursor.style.top = e.clientY + "px";
-});

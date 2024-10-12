38c736e2f02d47f4f51a8062f4f2b301f64e57b0
CodeGallantX <johnayobami77@proton.me>
2024-10-12 16:13:11 +0100

    added dark-mode toggle button

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,10 +1,8 @@
-function toggleDarkMode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
+// function toggleDarkMode() {
+//     var element = document.body;
+//     element.classList.toggle("dark-mode");
+// }
 
-    // if (element.classList.contains("dark-mode")) {
-    //     localStorage.setItem("darkMode", "true");
-    // } else {
-    //     localStorage.setItem("darkMode", "false");
-    // }
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
 }

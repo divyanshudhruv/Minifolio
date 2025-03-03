0167393c538d2cd01079df4bc44b42c06450c167
_v3 <116607327+marsianjohncarter@users.noreply.github.com>
2025-03-03 10:05:38 -0800

    Add responsive navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,3 +1,7 @@
+'use client';
+
+import { useState } from 'react';
+
 import "./navbar.css";
 
 interface Link {
@@ -6,20 +10,33 @@ interface Link {
 }
 
 export default function Navbar({ links }: { links: Link[] }) {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+  
+    const toggleMenu = () => {
+      setIsMenuOpen(!isMenuOpen);
+    };
+    
+    const handleLinkClick = () => {
+      setIsMenuOpen(false);
+    };
+    
     return (
-        <nav className="navbar">
-            <div className="navbar-logo">MINIFOLIO</div>
-            <div className="navbar-right">
-                <ul>
-                    <ul id="navbarNoDropdown" className="a">
-                        {links.map((link) => (
-                            <li key={`link-${link.name}`}>
-                                <a href={link.url}>{link.name}</a>
-                            </li>
-                        ))}
-                    </ul>
-                </ul>
-            </div>
-        </nav>
+      <nav className="navbar">
+        <div className="navbar-logo">MINIFOLIO</div>
+        <div className="navbar-right">
+          <ul id="navbarNoDropdown" className={isMenuOpen ? 'active' : ''}>
+            {links.map((link) => (
+              <li key={`link-${link.name}`}>
+                <a href={link.url} onClick={handleLinkClick}>{link.name}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className={`hamburger ${isMenuOpen ? 'active' : ''}`} onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </nav>
     );
 }
